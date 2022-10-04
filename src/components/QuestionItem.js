5a72aff7844360065ab3d140467c00e6f9804d26
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-10-04 11:08:02 -0400

    done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ onDelete, question }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'DELETE',
+    }).then(r => r.json()).then(() => onDelete(id))
+  }
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -17,7 +23,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
